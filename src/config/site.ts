8d6e5067dc264d75d211aaa2570a11efb10e9e95
cfngc4594 <cfngc4594@gmail.com>
2025-02-21 00:07:34 +0800

    feat(config): add site configuration with GitHub repository URL

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  url: {
+    repo: {
+      github: "https://github.com/cfngc4594/monaco-editor-lsp-next",
+    },
+  },
+};
+  
+export type SiteConfig = typeof siteConfig;
