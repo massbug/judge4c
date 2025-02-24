5cabdda667b6e5d5c5fdaea08b0c982f74309f0e
cfngc4594 <cfngc4594@gmail.com>
2025-02-24 13:17:16 +0800

    feat(store): add editor state to useCodeEditor store

--- src/store/useCodeEditor.ts
+++ src/store/useCodeEditor.ts
@@ -6,15 +6,19 @@ import { SupportedLanguage } from "@/constants/language";
 import { MonacoLanguageClient } from "monaco-languageclient";
 
 interface CodeEditorState {
+  editor: monaco.editor.IStandaloneCodeEditor | null;
   language: SupportedLanguage;
   languageClient: MonacoLanguageClient | null;
+  setEditor: (editor: monaco.editor.IStandaloneCodeEditor | null) => void;
   setLanguage: (language: SupportedLanguage) => void;
   setLanguageClient: (languageClient: MonacoLanguageClient | null) => void;
 }
 
 export const useCodeEditorState = create<CodeEditorState>((set) => ({
+  editor: null,
   language: DEFAULT_LANGUAGE,
   languageClient: null,
+  setEditor: (editor) => set({ editor }),
   setLanguage: (language) => set({ language }),
   setLanguageClient: (languageClient) => set({ languageClient }),
 }));
@@ -23,5 +27,5 @@ export const useCodeEditorOption = create<monaco.editor.IEditorConstructionOptio
   fontSize: CODE_EDITOR_OPTIONS.fontSize,
   lineHeight: CODE_EDITOR_OPTIONS.lineHeight,
   setFontSize: (fontSize: number) => set({ fontSize }),
-  setLineHeight: (lineHeight: number) => set({ lineHeight })
-}))
+  setLineHeight: (lineHeight: number) => set({ lineHeight }),
+}));
