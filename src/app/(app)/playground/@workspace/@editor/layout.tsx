543355722533c9a79090a99b7ffd250de73288eb
cfngc4594 <cfngc4594@gmail.com>
2025-02-25 22:08:38 +0800

    feat(editor): add WorkspaceEditorHeader to WorkspaceEditorLayout

--- src/app/(app)/playground/@workspace/@editor/layout.tsx
+++ src/app/(app)/playground/@workspace/@editor/layout.tsx
@@ -1,12 +1,16 @@
+import WorkspaceEditorHeader from "@/app/(app)/playground/@workspace/@editor/components/header";
 import WorkspaceEditorFooter from "@/app/(app)/playground/@workspace/@editor/components/footer";
 
 interface WorkspaceEditorLayoutProps {
   children: React.ReactNode;
 }
 
-export default function WorkspaceEditorLayout({ children }: WorkspaceEditorLayoutProps) {
+export default function WorkspaceEditorLayout({
+  children,
+}: WorkspaceEditorLayoutProps) {
   return (
     <div className="h-full flex flex-col">
+      <WorkspaceEditorHeader />
       {children}
       <WorkspaceEditorFooter />
     </div>
