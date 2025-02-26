1957a6baa00123260c2e21a045a01071b80a063a
cfngc4594 <cfngc4594@gmail.com>
2025-02-26 21:18:33 +0800

    feat(next.config): add dockerode to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  output: "standalone",
+  serverExternalPackages: ["dockerode"],
 };
 
 export default nextConfig;
