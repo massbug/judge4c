46da804f14fc6b3a94322c2c9028ad0e30ef4e5a
cfngc4594 <cfngc4594@gmail.com>
2025-02-26 21:54:54 +0800

    feat(layout): replace Banner component with Header component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,4 @@
-import { Banner } from "@/components/banner";
+import { Header } from "@/components/header";
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -7,7 +7,7 @@ interface AppLayoutProps {
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="h-full flex flex-col">
-      <Banner />
+      <Header />
       <main className="flex-1">
         {children}
       </main>
