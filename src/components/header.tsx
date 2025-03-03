eab87430950f1b6e33948cc7f430d2c3f2ad61f5
cfngc4594 <cfngc4594@gmail.com>
2025-03-03 11:40:05 +0800

    feat(header): add settings button to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import RunCode from "./run-code";
+import SettingsButton from "./settings-button";
 
 interface HeaderProps {
   className?: string;
@@ -12,9 +13,25 @@ export function Header({
   return (
     <header
       {...props}
-      className={cn("h-12 flex flex-none items-center justify-center", className)}
+      className={cn("relative", className)}
     >
-      <RunCode />
+      <nav className="relative h-12 w-full flex shrink-0 items-center px-2.5">
+        <div className="w-full flex justify-between">
+          <div className="w-full flex items-center justify-between">
+            <div className="flex items-center"></div>
+            <div className="relative flex items-center gap-2">
+              <SettingsButton />
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="z-10 absolute left-1/2 top-0 h-full -translate-x-1/2 py-2">
+        <div className="relative flex">
+          <div className="relative flex overflow-hidden rounded">
+            <RunCode />
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
