1d81013122cd95af9e07fdaa8889f623ac5a255c
cfngc4594 <cfngc4594@gmail.com>
2025-03-03 11:39:02 +0800

    feat(components): add settings button component with tooltip

---
+++ src/components/settings-button.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { SettingsIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+
+interface SettingsButtonProps {
+  className?: string;
+}
+
+export default function SettingsButton({
+  className,
+  ...props
+}: SettingsButtonProps) {
+  return (
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            className={cn("h-8 w-auto p-2",className)}
+            {...props}
+          >
+            <SettingsIcon size={16} aria-hidden="true" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="px-2 py-1 text-xs">Settings</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
