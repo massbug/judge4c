6ebf8fe935d2d50aed78330fb2253b070e94bd6c
cfngc4594 <cfngc4594@gmail.com>
2025-03-03 18:36:22 +0800

    refactor(problem-submission-page): simplify result destructuring and memo dependencies

--- src/app/(app)/playground/@problem/@submission/page.tsx
+++ src/app/(app)/playground/@problem/@submission/page.tsx
@@ -2,14 +2,22 @@
 
 import { useMemo } from 'react';
 import MdxPreview from "@/components/mdx-preview";
-import { useCodeEditorState } from "@/store/useCodeEditor";
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
 
 export default function ProblemSubmissionPage() {
-  const { result } = useCodeEditorState();
+  const { result } = useCodeEditorStore();
+  const { exitCode, output, executionTime, memoryUsage } = result || {};
 
   const template = useMemo(() => {
-    return `\`\`\`bash\n${result || ""}\n\`\`\``;
-  }, [result]);
+    return `
+    ${exitCode || ""}
+    \`\`\`bash
+    ${output || ""}
+    \`\`\`
+    ${executionTime || ""}
+    ${memoryUsage || ""}
+    `;
+  }, [exitCode, output, executionTime, memoryUsage]);
 
   return <MdxPreview source={template} />;
 }
