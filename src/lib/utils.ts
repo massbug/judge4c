a920cbc4b885212eb13d32ad67ca075195e7f1c4
cfngc4594 <cfngc4594@gmail.com>
2025-03-04 21:28:47 +0800

    refactor(utils): rename languageServerConfigs to LanguageServerConfig

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,13 @@
 import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
 import { EditorLanguage } from "@/types/editor-language";
-import languageServerConfigs from "@/config/language-server";
+import LanguageServerConfig from "@/config/language-server";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function getPath(lang: EditorLanguage): string {
-  const config = languageServerConfigs[lang];
+  const config = LanguageServerConfig[lang];
   return `file:///${config.lang.fileName}${config.lang.fileExtension}`;
 }
