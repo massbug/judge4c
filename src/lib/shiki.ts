1f417fb4e62268fde81392182a9012873df262fc
cfngc4594 <cfngc4594@gmail.com>
2025-03-04 20:29:21 +0800

    feat(shiki): improve Shiki highlighter initialization with dynamic theme and language support

--- src/lib/shiki.ts
+++ src/lib/shiki.ts
@@ -1,12 +1,23 @@
+import { MonacoTheme } from "@/types/monaco-theme";
 import { createHighlighter, Highlighter } from "shiki";
+import { EditorLanguage } from "@/types/editor-language";
 
+// Get all values from the ProgrammingLanguage and Theme enums
+const themes = Object.values(MonacoTheme);
+const languages = Object.values(EditorLanguage);
+
+// Use lazy initialization for highlighter
 let highlighter: Highlighter;
 
 async function initializeHighlighter() {
-  highlighter = await createHighlighter({
-    themes: ["github-light-default", "github-dark-default"],
-    langs: ["c"],
-  });
+  try {
+    highlighter = await createHighlighter({
+      themes: themes, // Use all values from the Theme enum
+      langs: languages, // Use all values from the ProgrammingLanguage enum
+    });
+  } catch (error) {
+    console.error("Error initializing highlighter:", error);
+  }
 }
 
 initializeHighlighter();
