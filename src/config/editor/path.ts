753422ebf448ff792de9b4c41490447729b57cc1
cfngc4594 <cfngc4594@gmail.com>
2025-03-05 08:28:52 +0800

    refactor: remove unused editor and language config files

--- src/config/editor/path.ts
+++
@@ -1,6 +0,0 @@
-import { SupportedLanguage } from "@/constants/language";
-
-export const DEFAULT_EDITOR_PATH: Record<SupportedLanguage, string> = {
-  c: "file:///main.c",
-  cpp: "file:///main.cpp",
-};
