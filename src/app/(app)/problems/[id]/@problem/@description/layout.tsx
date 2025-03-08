c6ab92e1e739b7c88ffd7bafe675bde5685afd55
cfngc4594 <cfngc4594@gmail.com>
2025-03-08 19:44:45 +0800

    feat(problems): add footer to problem description layout

---
+++ src/app/(app)/problems/[id]/@problem/@description/layout.tsx
@@ -0,0 +1,16 @@
+import ProblemDescriptionFooter from "@/features/workspace/problem/description/footer";
+
+interface ProblemDescriptionLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ProblemDescriptionLayout({
+  children,
+}: ProblemDescriptionLayoutProps) {
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex-1">{children}</div>
+      <ProblemDescriptionFooter />
+    </div>
+  );
+}
