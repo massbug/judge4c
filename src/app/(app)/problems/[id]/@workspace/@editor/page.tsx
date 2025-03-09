63bde8cafb0a32d3fcb04f22e3681353584c70e7
cfngc4594 <cfngc4594@gmail.com>
2025-03-09 10:49:34 +0800

    feat(editor): fetch problem templates from database

--- src/app/(app)/problems/[id]/@workspace/@editor/page.tsx
+++ src/app/(app)/problems/[id]/@workspace/@editor/page.tsx
@@ -1,5 +1,28 @@
+import prisma from "@/lib/prisma";
 import CodeEditor from "@/components/code-editor";
 
-export default function WorkspaceEditorPage() {
-  return <CodeEditor />;
+interface WorkspaceEditorProps {
+  params: Promise<{ id: string }>
+}
+
+export default async function WorkspaceEditorPage({
+  params,
+}: WorkspaceEditorProps) {
+  const { id } = await params;
+
+  const problem = await prisma.problem.findUnique({
+    where: { id: parseInt(id) },
+    select: {
+      templates: {
+        select: {
+          language: true,
+          template: true,
+        }
+      }
+    }
+  });
+
+  const templates = problem?.templates ?? [];
+
+  return <CodeEditor problemId={id} templates={templates} />;
 }
