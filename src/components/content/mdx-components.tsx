aaa4c1def5cb6082b415f34c06c806007b2ed1b9
cfngc4594 <cfngc4594@gmail.com>
2025-03-10 20:34:10 +0800

    fix(mdx-components): restore list decoration for ordered and unordered lists
    - Restored default list decorations (bullet points for `ul` and numbers for `ol`).
    - Solution based on issue from `sindresorhus/github-markdown-css#103`.

--- src/components/content/mdx-components.tsx
+++ src/components/content/mdx-components.tsx
@@ -3,4 +3,6 @@ import { Pre } from "@/components/content/pre";
 
 export const MdxComponents = {
   pre: (props: HTMLAttributes<HTMLPreElement>) => <Pre {...props} />,
+  ol: (props: HTMLAttributes<HTMLOListElement>) => <ol style={{ listStyle: "revert" }} {...props} />,
+  ul: (props: HTMLAttributes<HTMLUListElement>) => <ul style={{ listStyle: "revert" }} {...props} />,
 };
