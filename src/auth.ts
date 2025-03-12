89d9dbd01ea8632772c18c2c8c36c442491b7d5f
cfngc4594 <cfngc4594@gmail.com>
2025-03-12 15:18:31 +0800

    feat(auth): add GitHub provider for authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
 import { prisma } from "@/lib/prisma";
+import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [GitHub],
 });
