e6b4be475851cfc7eb60f6699ebc49b5d940ddbe
cfngc4594 <cfngc4594@gmail.com>
2025-03-12 21:17:14 +0800

    feat(problemset-layout): add AvatarButton to problemset layout

--- src/app/(app)/problemset/layout.tsx
+++ src/app/(app)/problemset/layout.tsx
@@ -1,4 +1,5 @@
 import { Banner } from "@/components/banner";
+import { AvatarButton } from "@/components/avatar-button";
 
 interface ProblemsetLayoutProps {
   children: React.ReactNode;
@@ -6,8 +7,11 @@ interface ProblemsetLayoutProps {
 
 export default function ProblemsetLayout({ children }: ProblemsetLayoutProps) {
   return (
-    <div className="flex flex-col">
+    <div className="relative flex flex-col">
       <Banner />
+      <div className="absolute top-2 right-4">
+        <AvatarButton />
+      </div>
       <main className="container mx-auto p-4">
         {children}
       </main>
