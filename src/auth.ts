256a8614e39e79a80b88425171f3e5ef75ab5d05
cfngc4594 <cfngc4594@gmail.com>
2025-03-12 15:07:20 +0800

    feat(auth): integrate PrismaAdapter with NextAuth for database integration

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+import NextAuth from "next-auth";
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 });
