97405a86577265ac474358a3174efea67edb676d
cfngc4594 <cfngc4594@gmail.com>
2025-03-12 12:22:41 +0800

    feat(components): add back button component with tooltip

---
+++ src/components/back-button.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { ArrowLeftIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+interface BackButtonProps {
+  href: string;
+  className?: string;
+}
+
+export default function BackButton({
+  href,
+  className,
+  ...props
+}: BackButtonProps) {
+  return (
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            className={cn("h-8 w-auto p-2", className)}
+            asChild
+            {...props}
+          >
+            <Link href={href}>
+              <ArrowLeftIcon size={16} aria-hidden="true" />
+            </Link>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="px-2 py-1 text-xs">Back</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
