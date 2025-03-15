672b627ed19d2bdabcefcdbefc49589d99447a4f
cfngc4594 <cfngc4594@gmail.com>
2025-03-15 22:34:23 +0800

    chore(app): remove playground folder

--- src/app/(app)/playground/@problem/@description/page.tsx
+++
@@ -1,6 +0,0 @@
-import MdxPreview from "@/components/mdx-preview";
-import { DEFAULT_PROBLEM_DESCRIPTION } from "@/config/problem/description";
-
-export default function ProblemDescriptionPage() {
-  return <MdxPreview source={DEFAULT_PROBLEM_DESCRIPTION} />;
-}
--- src/app/(app)/playground/@workspace/@editor/page.tsx
+++
@@ -1,6 +0,0 @@
-// import CodeEditor from "@/components/code-editor";
-
-export default function WorkspaceEditorPage() {
-  // return <CodeEditor />;
-  return null;
-}
