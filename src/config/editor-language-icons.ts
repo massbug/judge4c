02f4fc1c3f40bbd94c476c5fe0f63eb607d77d4e
cfngc4594 <cfngc4594@gmail.com>
2025-03-16 13:31:08 +0800

    feat(config): add mapping for EditorLanguage to icons

---
+++ src/config/editor-language-icons.ts
@@ -0,0 +1,11 @@
+import { EditorLanguage } from "@prisma/client";
+import { COriginal, CplusplusOriginal } from "devicons-react";
+
+// Mapping between EditorLanguage and icons
+export const EditorLanguageIcons: Record<
+  EditorLanguage,
+  React.FunctionComponent<React.SVGProps<SVGElement> & { size?: number | string }>
+> = {
+  [EditorLanguage.c]: COriginal,
+  [EditorLanguage.cpp]: CplusplusOriginal,
+};
