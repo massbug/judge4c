b0bb5b5250ee213a9f9c7812023c0b969f9662e3
cfngc4594 <cfngc4594@gmail.com>
2025-03-16 14:27:28 +0800

    feat(editor-language): simplify language configuration to a constant

--- src/config/editor-language.ts
+++ src/config/editor-language.ts
@@ -1,23 +1,3 @@
 import { EditorLanguage } from "@prisma/client";
-import { EditorLanguageMetadata } from "@/types/editor-language";
 
-// Define language configurations
-const EditorLanguageConfig: Record<EditorLanguage, EditorLanguageMetadata> = {
-  [EditorLanguage.c]: {
-    id: EditorLanguage.c,
-    label: "C",
-    fileName: "main",
-    fileExtension: ".c",
-  },
-  [EditorLanguage.cpp]: {
-    id: EditorLanguage.cpp,
-    label: "C++",
-    fileName: "main",
-    fileExtension: ".cpp",
-  },
-};
-
-// Default language configuration
-const DefaultEditorLanguageConfig = EditorLanguageConfig[EditorLanguage.c]; // Default to C language
-
-export { EditorLanguageConfig, DefaultEditorLanguageConfig };
+export const DEFAULT_EDITOR_LANGUAGE = EditorLanguage.c;
