ff8eff58914a2e7a87207f877b8f4a052c7ce78f
cfngc4594 <cfngc4594@gmail.com>
2025-03-16 15:39:07 +0800

    chore(config, types): remove deprecated language server and editor language files

--- src/types/editor-language.ts
+++
@@ -1,8 +0,0 @@
-import { EditorLanguage } from "@prisma/client";
-
-export type EditorLanguageMetadata = {
-  id: EditorLanguage,
-  label: string;
-  fileName: string;
-  fileExtension: string;
-};
--- src/types/language-server.ts
+++
@@ -1,9 +0,0 @@
-import { EditorLanguageMetadata } from "./editor-language";
-
-export type LanguageServerMetadata = {
-  protocol: string;
-  hostname: string;
-  port: number | null;
-  path: string | null;
-  lang: EditorLanguageMetadata;
-};
