168df31fad02e2a37df5f35d9c566dd17901d7f9
cfngc4594 <cfngc4594@gmail.com>
2025-03-18 10:40:03 +0800

    refactor(admin): replace breadcrumb with Navbar in dashboard layout

--- src/app/(app)/dashboard/@admin/layout.tsx
+++ src/app/(app)/dashboard/@admin/layout.tsx
@@ -4,16 +4,9 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { auth } from "@/lib/auth";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { User } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { Navbar } from "@/components/navbar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Separator } from "@/components/ui/separator";
 import { type NavUserProps } from "@/components/nav-user";
@@ -40,31 +33,14 @@ export default async function AdminDashboardLayout({
   return (
     <SidebarProvider>
       <AppSidebar user={user} />
-
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-
-                <BreadcrumbSeparator className="hidden md:block" />
-
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <Navbar />
           </div>
         </header>
-
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           {children}
         </div>
