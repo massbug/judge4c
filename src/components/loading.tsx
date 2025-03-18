91ce73d72bfead3af4a5a6a66505e32914eab30f
cfngc4594 <cfngc4594@gmail.com>
2025-03-18 19:46:19 +0800

    feat(Loading): add skeletonClassName prop to customize Skeleton style

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,15 +3,17 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 interface LoadingProps {
   className?: string;
+  skeletonClassName?: string;
 }
 
 export function Loading({
   className,
+  skeletonClassName,
   ...props
 }: LoadingProps) {
   return (
     <div className={cn("h-full w-full p-2", className)} {...props}>
-      <Skeleton className="h-full w-full rounded-3xl" />
+      <Skeleton className={cn("h-full w-full rounded-3xl", skeletonClassName)} />
     </div>
   );
 }
