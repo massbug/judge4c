ef374513132c3736ee91b20ab131e293188d10ed
cfngc4594 <cfngc4594@gmail.com>
2025-03-24 21:27:12 +0800

    chore(ui): add eslint-disable for unused variables in ChatMessageList

--- src/components/ui/chat/chat-message-list.tsx
+++ src/components/ui/chat/chat-message-list.tsx
@@ -8,10 +8,12 @@ interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const ChatMessageList = React.forwardRef<HTMLDivElement, ChatMessageListProps>(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ({ className, children, smooth = false, ...props }, _ref) => {
     const {
       scrollRef,
       isAtBottom,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       autoScrollEnabled,
       scrollToBottom,
       disableAutoScroll,
