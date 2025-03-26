793db58e3621650e6eb434a6280c5dfac32fe02f
cfngc4594 <cfngc4594@gmail.com>
2025-03-26 22:48:00 +0800

    feat(mdx): add scroll support for tables

--- src/components/content/mdx-components.tsx
+++ src/components/content/mdx-components.tsx
@@ -1,8 +1,19 @@
 import { HTMLAttributes } from "react";
 import { Pre } from "@/components/content/pre";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export const MdxComponents = {
   pre: (props: HTMLAttributes<HTMLPreElement>) => <Pre {...props} />,
-  ol: (props: HTMLAttributes<HTMLOListElement>) => <ol style={{ listStyle: "revert" }} {...props} />,
-  ul: (props: HTMLAttributes<HTMLUListElement>) => <ul style={{ listStyle: "revert" }} {...props} />,
+  ol: (props: HTMLAttributes<HTMLOListElement>) => (
+    <ol style={{ listStyle: "revert" }} {...props} />
+  ),
+  ul: (props: HTMLAttributes<HTMLUListElement>) => (
+    <ul style={{ listStyle: "revert" }} {...props} />
+  ),
+  table: (props: HTMLAttributes<HTMLTableElement>) => (
+    <ScrollArea>
+      <table {...props} />
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  ),
 };
