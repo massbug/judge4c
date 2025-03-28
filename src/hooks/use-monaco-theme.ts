d8597967554a7a3c36684d3dd989467035f3d984
cfngc4594 <cfngc4594@gmail.com>
2025-03-28 13:08:14 +0800

    refactor(theme): update Monaco theme configuration to use default light and dark themes

--- src/hooks/use-monaco-theme.ts
+++ src/hooks/use-monaco-theme.ts
@@ -1,11 +1,10 @@
 import { useTheme } from "next-themes";
-import { MonacoTheme } from "@/types/monaco-theme";
-import { MonacoThemeConfig } from "@/config/monaco-theme";
+import { DefaultLightThemeConfig, DefaultDarkThemeConfig } from "@/config/monaco-theme";
 
 export function useMonacoTheme() {
   const { resolvedTheme } = useTheme();
 
-  const currentTheme = resolvedTheme === "light" ? MonacoThemeConfig[MonacoTheme.GitHubLightDefault].id : MonacoThemeConfig[MonacoTheme.GitHubDarkDefault].id;
+  const currentTheme = resolvedTheme === "light" ? DefaultLightThemeConfig.id : DefaultDarkThemeConfig.id;
 
   return {
     currentTheme,
