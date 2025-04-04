0d074a556d23ed11f957afd756e7ff1b5c1991a0
cfngc4594 <cfngc4594@gmail.com>
2025-04-04 21:13:12 +0800

    feat(store): add hydration logic and state management to new problem store

--- src/app/(app)/dashboard/@admin/problemset/new/store.ts
+++ src/app/(app)/dashboard/@admin/problemset/new/store.ts
@@ -2,18 +2,34 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { ProblemSchema } from "@/components/features/dashboard/admin/problemset/new/schema";
 
-type NewProblemState = Partial<ProblemSchema> & {
+interface NewProblemActions {
+  setHydrated: (value: boolean) => void;
   setData: (data: Partial<ProblemSchema>) => void;
-};
+}
+
+type NewProblemState = Partial<ProblemSchema> & {
+  hydrated: boolean;
+} & NewProblemActions;
 
 export const useNewProblemStore = create<NewProblemState>()(
   persist(
     (set) => ({
+      hydrated: false,
+      setHydrated: (value) => set({ hydrated: value }),
       setData: (data) => set(data),
     }),
     {
-      name: "new-problem-storage",
+      name: "zustand:new-problem",
       storage: createJSONStorage(() => localStorage),
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      partialize: ({ hydrated, ...rest }) => rest,
+      onRehydrateStorage: () => (state, error) => {
+        if (error) {
+          console.error("An error happened during hydration", error);
+        } else if (state) {
+          state.setHydrated(true);
+        }
+      },
     }
   )
 );
