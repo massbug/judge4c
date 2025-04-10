2f2c8ab1128139c5875a7499505bd6f8f8e07567
cfngc4594 <cfngc4594@gmail.com>
2025-04-10 10:41:36 +0800

    refactor(problems): improve testcase scroll layout using ScrollArea

--- src/app/(app)/problems/[id]/features/testcase.tsx
+++ src/app/(app)/problems/[id]/features/testcase.tsx
@@ -1,12 +1,17 @@
 import TestcaseCard from "@/components/testcase-card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useProblemStore } from "@/providers/problem-store-provider";
 
 export default function Testcase() {
   const problem = useProblemStore((state) => state.problem);
 
   return (
-    <div className="h-full rounded-b-3xl border border-t-0 border-muted bg-background">
-      <TestcaseCard testcases={problem.testcases} />
+    <div className="relative h-full border border-t-0 border-muted rounded-b-3xl bg-background">
+      <div className="absolute h-full w-full">
+        <ScrollArea className="h-full">
+          <TestcaseCard testcases={problem.testcases} />
+        </ScrollArea>
+      </div>
     </div>
   );
 }
